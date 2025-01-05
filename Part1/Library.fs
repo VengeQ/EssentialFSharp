5408e47cab73d88f5c389ef4daf1b2648a8aaf90
vengeq <vengeq@gmail.com>
2025-01-05 21:06:44 +1000

    part1

--- Part1/Library.fs
+++ Part1/Library.fs
@@ -1,5 +1,28 @@
 ﻿namespace Part1
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Persons =
+    //type Person = { Id: int; Name: string; LastName : string; IsEmployee : bool}
+
+    type Employee = { Id: int; Name: string; LastName : string }
+    type Customer = { Id: int; Name: string; LastName : string }
+    type Person = 
+        | Employee of Employee 
+        | Customer of Customer
+
+    let Name person = 
+        let name, lastName= 
+            match person with
+            | Employee e -> e.Name, e.LastName
+            | Customer c -> c.Name, c.LastName
+        name, lastName
+
+    let CalculatePrice person spend = 
+        let discountRatio =
+            match person with
+            | Employee _ ->
+                if spend > 10000m then 0.2m
+                else if spend > 1000m then 0.1m
+                else 0.0m
+            | Customer _ -> 0.0m
+        spend * (1m - discountRatio)
+            
--- Part1Tests/UnitTest1.fs
+++ Part1Tests/UnitTest1.fs
@@ -1,11 +1,45 @@
 module Part1Tests
 
 open NUnit.Framework
+open Part1.Persons
 
-[<SetUp>]
-let Setup () =
-    ()
+[<TestFixture>]
+type PersonsTests() = 
+    [<Test>]
+    member this.CheckMe () = 
+        let me = Employee { Id = 1; Name = "Daniil"; LastName = "MyFamily" }
+        let name, lastName = Name me
+        Assert.That(name, Is.EqualTo("Daniil"))
+        Assert.That(lastName, Is.EqualTo("MyFamily"))
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+    [<TestCase(800, 800)>]
+    [<TestCase(1000, 1000)>]
+    [<TestCase(1001, 900.9)>]
+    [<TestCase(5000, 4500)>]
+    [<TestCase(10000, 9000)>]
+    [<TestCase(10001, 8000.8)>]
+    [<TestCase(15569, 12455.2)>]
+    member this.CalculateEmployeePrice(spend : decimal, expected : decimal) = 
+        let employee = Employee { Id = 3; Name = "PersonName"; LastName = "PersonLastName"}
+        let actual = CalculatePrice employee spend
+        Assert.That(actual, Is.EqualTo(expected))
+
+    [<TestCase(800, 800)>]
+    [<TestCase(1000, 1000)>]
+    [<TestCase(1001, 1001)>]
+    [<TestCase(5000, 5000)>]
+    [<TestCase(10000, 10000)>]
+    [<TestCase(10001, 10001)>]
+    [<TestCase(15569, 15569)>]
+    member this.CalculateNotEmployeePrice(spend : decimal, expected : decimal) = 
+        let person = Customer { Id = 3; Name = "PersonName"; LastName = "PersonLastName" }
+        let actual = CalculatePrice person spend
+        Assert.That(actual, Is.EqualTo(expected))
+
+[<TestFixture>]
+type OtherTests() = 
+    [<Test>]
+    member this.Mutable () = 
+        let mutable value = 23
+        value <- 22
+        Assert.That(value, Is.EqualTo(22))
